eb55c67df5839cab0c1fa8a1c1efb891f78f6c32
poi2 <daisuke.ito.cs@gmail.com>
2016-03-28 00:32:34 +0900

    add decoder

--- square.rb
+++ square.rb
@@ -1,1 +1,13 @@
+# START: 3/28 00:13
+
 args = "3:Oh,Be,AF,in,eG,ir,l5,Q8,mC,7T,Ty,tT"
+element, *dots = args.split(/[:,]/)
+
+def euclidlize di_str
+  [numlize(di_str[0]), numlize(di_str[1])]
+end
+
+DECODER = [*("0".."9"), *("A".."Z"), *("a".."z")]
+def numlize str
+  DECODER.find_index(str)
+end
