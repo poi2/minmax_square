10b1fd23cf6c5b77ce4f95d070301ae19d31f2f5
poi2 <daisuke.ito.cs@gmail.com>
2016-03-29 00:43:27 +0900

    add calc_min_square_area

--- square.rb
+++ square.rb
@@ -1,5 +1,7 @@
 # 1 START: 3/28 00:13
 # 1 END:   3/28 01:10
+# 2 START: 3/28 23:37
+# 2 END:   3/29 00:35
 
 args = "3:Oh,Be,AF,in,eG,ir,l5,Q8,mC,7T,Ty,tT"
 element, *dots = args.split(/[:,]/)
@@ -27,9 +29,41 @@ def min_wrapping_square_coordinate dots
   }
 end
 
-def enclosed? sq_hash, xy
+def square_to_area sq
+  (sq[:max_x] - sq[:min_x] + 1) * (sq[:max_y] - sq[:min_y] + 1)
+end
+
+def enclosed? sq, xy
+  # squareにxyが含まれるかどうか
   x, y = xy[0], xy[1]
-  return true if sq_hash[:min_x] <= x && x <= sq_hash[:max_x] &&
-                 sq_hash[:min_y] <= y && y <= sq_hash[:max_y]
+  return true if sq[:min_x] <= x && x <= sq[:max_x] &&
+                 sq[:min_y] <= y && y <= sq[:max_y]
   return false
+end
+
+def calc_min_square_area args
+  element, *dots = args.split(/[:,]/)
+  min_area = 62 * 62
+  min_cluster = nil
+  element = element.to_i
+
+  coords = dots.map{ |dot| euclidlize(dot) }
+  sets = coords.combination(element).to_a; nil
+
+  sets.each do |cluster|
+    sq = min_wrapping_square_coordinate(cluster)
+    area = square_to_area(sq)
+    if area < min_area
+      enclosed = (coords - cluster).any? do |xy|
+        enclosed?(sq, xy)
+      end
+      unless enclosed
+        min_area = area
+        min_cluster = cluster
+      end
+    end
+  end
+
+  p "min_area: #{min_area}"
+  p "min_cluster: #{min_cluster}"
 end
